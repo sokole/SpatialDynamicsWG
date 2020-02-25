01320e4d00acd84c067b4cac50b429ff893402b7
Eric R. Sokol <sokole@gmail.com>
2020-02-25 14:10:54 -0700

    update code to save a csv of the full raw data set to google drive

--- PopComm/RCODE_FISH/02_cleaning_fish_data.r
+++ PopComm/RCODE_FISH/02_cleaning_fish_data.r
@@ -2,7 +2,7 @@
 
 ####################################################################
 # -- Biodiversity (FISH) data cleaning script -- pop comm group -- Stream Resiliency RCN
-# -- -- updated 19 May 2019
+# -- -- updated 25 Feb 2020
 # -- -- Eric Sokol
 
 # clear out workspace
@@ -20,8 +20,14 @@ source('https://raw.githubusercontent.com/sokole/SpatialDynamicsWG/master/PopCom
 
 ####################
 # searching google drive for dirs from which to pull data
-my_path_to_googledirve_directory <- 'Spatial Dynamics WG/Pop-comm group/NAQWA_Biodata_All_NEW_November2018/FISH'
-my_list_of_files <- googledrive::drive_ls(my_path_to_googledirve_directory)
+my_drive_id <- '1WaSQJL21To63xN8R6AsE9EuHL2i3XrUl' #google drive id for fish dir
+# my_path_to_googledirve_directory <- 'Spatial Dynamics WG/Pop-comm group/NAQWA_Biodata_All_NEW_November2018/FISH'
+# found_dir <- googledrive::drive_find(pattern = my_path_to_googledirve_directory)
+
+# my_list_of_files <- googledrive::drive_ls(path = my_path_to_googledirve_directory)
+my_list_of_files <- my_drive_id %>%
+  googledrive::as_id() %>% 
+  googledrive::drive_ls()
 
 my_list_of_huc_dirs <- my_list_of_files %>% filter(grepl('HUC',name))
 
@@ -30,11 +36,13 @@ my_list_of_huc_dirs <- my_list_of_files %>% filter(grepl('HUC',name))
 for(i_huc in 1:nrow(my_list_of_huc_dirs)){
   
   # get dir path for i_huc
-  i_huc_dir_path <- paste0(my_path_to_googledirve_directory, '/', my_list_of_huc_dirs$name[i_huc])
+  # i_huc_dir_path <- paste0(my_path_to_googledirve_directory, '/', my_list_of_huc_dirs$name[i_huc])
+  # i_huc_dir_path <- my_list_of_huc_dirs$id[i_huc] %>% googledrive::as_id()
+  i_huc_googleid <- my_list_of_huc_dirs$id[i_huc] %>% googledrive::as_id()
   
   # check i_huc_dir_path for expected xlsx file
   i_huc_list_of_files <- data.frame() #initialize as data.frame with 0 rows
-  i_huc_list_of_files <- googledrive::drive_ls(i_huc_dir_path)
+  i_huc_list_of_files <- googledrive::drive_ls(i_huc_googleid)
   
   xlsx_file_list <- data.frame() #initialize as data.frame with 0 rows
   xlsx_file_list <- i_huc_list_of_files %>% filter(grepl('(?i)results\\.xlsx', name))
@@ -48,7 +56,8 @@ for(i_huc in 1:nrow(my_list_of_huc_dirs)){
   # function to clean data
   try({
     clean_data_fish_nawqa(
-      my_path_to_googledirve_directory = i_huc_dir_path,
+      # my_path_to_googledirve_directory = i_huc_dir_path,
+      my_google_drive_directory_id = i_huc_googleid,
       keep_local_output = FALSE)
   })
 }
