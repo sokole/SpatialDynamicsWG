009aa56054dc1e5d03ddb06a7d1c86fe62d17dd8
Darin Kopp <33871896+dkopp3@users.noreply.github.com>
2020-04-16 21:02:00 -0500

    site revisits?

--- PopComm/RCODE_MACROINVERTS/02_cleaning_data_invert.R
+++ PopComm/RCODE_MACROINVERTS/02_cleaning_data_invert.R
@@ -1,6 +1,6 @@
 ####################################################################
 # -- Biodiversity Macroinvert data cleaning script -- pop comm group -- Stream Resiliency RCN
-# -- -- Eric Sokol and Darin 
+# -- -- Eric Sokol and Darin Kopp 
 # -- -- updated 15 April 2020
 
 # clear out workspace
@@ -75,163 +75,9 @@ aggregate(sites$SiteNumber, by = list(sites$SiteNumber), length)
 
 ################################################################
 # -- 4. standardize taxonomic resolution
-# probably dont need to re run saved all 
-# taxa crosswalk to google drive 
+# need itis_taxa_match created in Standardize_Taxa.r
 ##########################################
 
-####################
-# check valid name (tsn) for target taxa
-#install.packages("taxize")
-library(taxize)
-
-####################
-# select major taxa levels
-
-dat_full_tax <- dat_all %>% 
-  select(c(SiteNumber, ScientificName, Phylum, 
-           Class,  Order, Family, Genus))
-
-####################
-# Identify lowest taxa level 
-
-dat_full_tax$low_taxid <- apply(dat_full_tax, 1, function(x) 
-  names(x)[max(which(!is.na(x)))])
-
-####################
-# Sciname of lowest level level, target_tax is primary key
-dat_full_tax$target_tax <- apply(dat_full_tax, 1, function(x) x[x["low_taxid"]])
-
-####################
-# TSN list to check 
-check_tsn <- unique(dat_full_tax[,c("target_tax","low_taxid")])
-
-####################
-# loop target_tax
-# "Error: Not Found (HTTP 404)" came and went
-# took about 1hr to match 797 taxa
-
-tsns <- data.frame()
-for (i in 1:nrow(check_tsn)){
-  
-  #invalid/unaccepted returns NA
-  my_tsn <- taxize::get_tsn(check_tsn$target_tax[i], 
-                            accepted = T,
-                            messages = F)
-  tsns <- rbind(tsns, data.frame(tsn = my_tsn[1], 
-                                 target_tax = check_tsn$target_tax[i]))
-}
-
-tsns <- merge(tsns, check_tsn, by = "target_tax")
-tsns$acceptedname <- tsns$target_tax
-tsns$acceptedtsn <- tsns$tsn
-tsns$acceptedrank <- tsns$low_taxid
-valid <- tsns[,-3]
-
-
-####################
-# check invalid names (NA values in valid)
-
-invalid <- valid[is.na(valid$tsn), ]
-
-out <- data.frame()
-for (i in 1:nrow(invalid)){
-  my_tsn <- taxize::get_tsn(invalid$target_tax[i], messages = F)
-  
-  # some taxa are unmatched in itis - no record
-  if(!is.na(my_tsn[1])){
-    
-    # get accepted names & rank
-    accepted_name <- itis_acceptname(my_tsn[1])
-    rank <- itis_taxrank(accepted_name$acceptedtsn[1])
-    
-    # can have multiple valid names if, for example, 
-    # a class was dissolved into two phlya
-    temp <- data.frame(invalid[i, ], accepted_name[1, c("acceptedname", "acceptedtsn")], acceptedrank = rank)
-    out <- rbind(out, temp)
-    
-  } else {
-    
-    #some taxa do not have valid name
-    temp <- data.frame(invalid[i, ], acceptedname = NA, acceptedtsn = NA, acceptedrank = NA)
-    out <- rbind(out, temp)
-    
-  }
-}
-
-# invalid taxa
-invalid <- out
-
-
-####################
-# unmatched taxa (those with no matching record in itis)
-# selected the next highest level 
-
-unmatched_taxa <- invalid[is.na(invalid$acceptedname),]
-
-#Identify next highest taxa level
-unmatched_taxa <- unique(dat_full_tax[dat_full_tax$target_tax %in% unmatched_taxa$taxname, 
-                                      c("Phylum", "Class", "Order", "Family", 
-                                        "Genus", "low_taxid", "target_tax")])
-new_tax <- apply(unmatched_taxa, 1, 
-                 function (x) data.frame(target_tax = x["target_tax"], 
-                                         taxname = x[which(names(x) == as.character(x["low_taxid"])) - 1]))
-new_tax <- do.call(rbind, new_tax)
-
-####################
-#loop through unmatched taxa at newx highest level
-out <- data.frame()
-for (i in 1:length(unique(new_tax$taxname))){
-  #i<-3
-  my_tsn <- taxize::get_tsn(unique(new_tax$taxname)[i], messages = F)
-  
-  if(!is.na(my_tsn[1])){
-    
-    # get accepted names & rank
-    accepted_name <- itis_acceptname(my_tsn[1])
-    rank <- itis_taxrank(accepted_name$acceptedtsn[1])
-    
-    # can have multiple valid names if, for example, 
-    # a class was dissolved into two phlya
-    # choose the first
-    temp <- data.frame(taxname = unique(new_tax$taxname)[i], accepted_name[1, c("acceptedname", "acceptedtsn")], acceptedrank = rank)
-    out <- rbind(out, temp)
-    
-  } else {
-    #some tase do not have valid name
-    temp <- data.frame(taxname = unique(new_tax$taxname)[i], acceptedname = NA, acceptedtsn = NA, acceptedrank = NA)
-    out <- rbind(out, temp)
-  }
-}
-
-new_tax <- merge(new_tax, out, by = "taxname")
-new_tax[is.na(new_tax$acceptedname), "acceptedname"] <- new_tax[is.na(new_tax$acceptedname), "taxname"]
-
-unmatched_taxa <- new_tax[,-1]
-unmatched_taxa$tsn <- NA
-
-
-
-####################
-# create updated itis_taxa_match
-
-valid <- valid[!valid$target_tax%in%invalid$target_tax, ]
-valid$itismatch <- "valid"
-invalid <- invalid[!invalid$target_tax %in% unmatched_taxa$target_tax, ]
-invalid$itismatch<-"invalid"
-unmatched_taxa$itismatch<-"unmatched"
-itis_taxa_match <- do.call(rbind,list(valid, invalid, unmatched_taxa))
-
-####################
-#write itis match to google
-write_to_google_drive(
-  data_to_write = itis_taxa_match,
-  write_filename = paste0('itis_taxa_match_', taxon_group, '.csv'),
-  my_path_to_googledirve_directory = my_drive_id %>%
-    googledrive::as_id(),
-  keep_local_copy_of_file = FALSE)
-
-
-####################
 # Identify distinct taxa: 
 # "Third, individuals not identified to the targeted level
 # of taxonomy (hereafter, ambiguous taxa) were resolved by 
@@ -291,13 +137,12 @@ fu <- function(x){
 }
 
 
-length(unique(dat_all$SiteVisitSampleNumber))
+#kept all site and site visits separate may want to change
 dat_all$splitID <- paste0(dat_all$SiteNumber, "-", dat_all$SiteVisitSampleNumber)
 distinct_site_taxa <- split(dat_all, factor(dat_all$splitID))
 
 distinct_site_taxa <- lapply(distinct_site_taxa, function(x) fu(x))
 distinct_site_taxa <- do.call(rbind, distinct_site_taxa)
-dim(distinct_site_taxa)
 
 ################################################################
 # -- 5. filter out rare taxa
